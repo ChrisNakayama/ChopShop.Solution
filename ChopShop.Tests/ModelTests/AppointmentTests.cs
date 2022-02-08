b28d4d4718e163256caee6f2b1761e8ba119bd67
ChrisNakayama <88105917+ChrisNakayama@users.noreply.github.com>
2022-02-07 20:36:45 -0800

    Dryed up code
    Co-authored by: Phil Curran <pecurran@hotmail.com>
    Co-authored by: <johnwhitten.studio@gmail.com>
    Co-authored by: <ejmcculley@gmail.com>.

--- ChopShop.Tests/ModelTests/AppointmentTests.cs
+++ ChopShop.Tests/ModelTests/AppointmentTests.cs
@@ -9,34 +9,39 @@ namespace ChopShop.Tests
   public class AppointmentTests
   {
     [TestMethod]
-    public void CheckIfAppointmentExists() 
-    {
-      Appointment testAppointment = new Appointment("name", "time", "service");
-      Assert.AreEqual(typeof(Appointment), testAppointment.GetType());
-    }
+      public void CreateNewAppointment()
+      {
+        Appointment newAppointment = new Appointment("name", "time", "service");
+        Assert.AreEqual(typeof(Appointment), newAppointment.GetType());
+      }
+//     public void CheckIfAppointmentExists() 
+//     {
+//       Appointment testAppointment = new Appointment("name", "time", "service");
+//       Assert.AreEqual(typeof(Appointment), testAppointment.GetType());
+//     }
 
-    [TestMethod]
-    public void CheckAppointmentName() 
-    {
-      string name = "Test Name";
-      Appointment testAppointment = new Appointment(name, "time", "service");
-      Assert.AreEqual(testAppointment.Name, name);
-    }
+//     [TestMethod]
+//     public void CheckAppointmentName() 
+//     {
+//       string name = "Test Name";
+//       Appointment testAppointment = new Appointment(name, "time", "service");
+//       Assert.AreEqual(testAppointment.Name, name);
+//     }
 
-    [TestMethod]
-    public void CheckAppointmentTime() 
-    {
-      string time = "Test Time";
-      Appointment testAppointment = new Appointment("name", time, "service");
-      Assert.AreEqual(testAppointment.Time, time);
-    }
+//     [TestMethod]
+//     public void CheckAppointmentTime() 
+//     {
+//       string time = "Test Time";
+//       Appointment testAppointment = new Appointment("name", time, "service");
+//       Assert.AreEqual(testAppointment.Time, time);
+//     }
 
-    [TestMethod]
-    public void CheckAppointmentService() 
-    {
-      string service = "Test Service";
-      Appointment testAppointment = new Appointment("name", "time", service);
-      Assert.AreEqual(testAppointment.Service, service);
-    }
+//     [TestMethod]
+//     public void CheckAppointmentService() 
+//     {
+//       string service = "Test Service";
+//       Appointment testAppointment = new Appointment("name", "time", service);
+//       Assert.AreEqual(testAppointment.Service, service);
+//     }
   }
 }
